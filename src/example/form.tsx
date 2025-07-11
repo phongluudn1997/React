576861f98e764f23486fe5c3818b67c9b3c904f3
lphuynh <luu.phong.huynh@mgm-tp.com>
2025-07-11 23:28:55 +0700

    feat: useDebounce in Form

--- src/example/form.tsx
+++ src/example/form.tsx
@@ -1,41 +1,62 @@
-import {type ChangeEvent, type FormEventHandler, useState} from "react";
+import {
+  type ChangeEvent,
+  type FormEventHandler,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import { useDebounce } from "../hooks/useDebounce";
 
 /**
  * Controlled Component
  * Handle debounce
  */
 export const Form = () => {
-    const [formData, setFormData] = useState({
-        username: "",
-        password: ""
-    })
+  const submitForm = () => {
+    console.log(formData);
+  };
 
-    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-        setFormData({
-            ...formData,
-            [event.target.name]: event.target.value
-        })
-    }
+  const debouncedSubmitForm = useDebounce(submitForm);
 
-    const handleSubmit: FormEventHandler = (event) => {
-        event.preventDefault();
-        debounceFunction()
-    }
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
 
-    const debounceFunction = () => {
-        console.log(formData)
-    }
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
 
+  const handleSubmit: FormEventHandler = (event) => {
+    event.preventDefault();
+    // Call the last handler
+    debouncedSubmitForm();
+  };
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor={"username"}>Username</label>
-            <input type="text" id={"username"} name={"username"} value={formData.username} onChange={handleChange}/>
-            <br/>
-            <label htmlFor={"password"}>Password</label>
-            <input type="password" id={"password"} name={"password"} value={formData.password} onChange={handleChange}/>
-            <br/>
-            <button type="submit">Submit</button>
-        </form>
-    )
-}
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor={"username"}>Username</label>
+      <input
+        type="text"
+        id={"username"}
+        name={"username"}
+        value={formData.username}
+        onChange={handleChange}
+      />
+      <br />
+      <label htmlFor={"password"}>Password</label>
+      <input
+        type="password"
+        id={"password"}
+        name={"password"}
+        value={formData.password}
+        onChange={handleChange}
+      />
+      <br />
+      <input type="submit" />
+    </form>
+  );
+};
