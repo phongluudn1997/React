a723ed41b1fe78fc3ef0a1ab360a5eb2620368f1
lphuynh <luu.phong.huynh@mgm-tp.com>
2025-07-11 23:29:03 +0700

    feat: add debounce

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -1,4 +1,4 @@
-export const debounce = <T extends unknown[]>(func: (...args: T) => void, delay: number) => {
+export const debounce = <T extends unknown[]>(func: (...args: T) => void, delay: number = 1000) => {
     let timeoutId: ReturnType<typeof setTimeout>;
     return function (...args: T) {
         clearTimeout(timeoutId)
