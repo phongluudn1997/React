1c7d0d084e63abfefcafd7b58bb2cd63241af963
lphuynh <luu.phong.huynh@mgm-tp.com>
2025-07-11 09:48:24 +0700

    feat: add useCounter

--- src/hooks/useCounter.ts
+++ src/hooks/useCounter.ts
@@ -1,19 +1,21 @@
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 
 /**
  * Since count is a reactive value, it must be specified in the list of dependencies. However, that causes the Effect to cleanup and setup again every time the count changes. This is not ideal.
  * To fix this, pass the c => c + 1 state updater to setCount:
  */
-function useCounter() {
-  const [counter, setCounter] = useState<number>(0);
+export const useCounter = () => {
+    const [counter, setCounter] = useState<number>(0);
 
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setCounter(counter => counter + 1);
-    }, 1000);
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCounter((counter) => counter + 1);
+        }, 1000);
 
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, []);
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, []);
+
+    return {counter}
 }
