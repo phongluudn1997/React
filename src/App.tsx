09f5923c09b5d45279659d0f020685099929219f
lphuynh <huynhpluu@gmail.com>
2025-07-31 18:54:10 +0700

    feat: update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import './App.css'
-import {FilteredProductTable} from "./example/product-table/FilteredProducTable.tsx";
-import {PRODUCTS} from "./example/product-table/data.tsx";
+import {DebouncedSearchInput} from "./example/debounceSearchInput.tsx";
 
 function App() {
     return (
-        <>
-            <FilteredProductTable products={PRODUCTS}/>
-        </>
+        <DebouncedSearchInput/>
     )
 }
 
