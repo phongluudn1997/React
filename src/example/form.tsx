09272b7097412f83619cd3410502d0a1ffc35e75
lphuynh <huynhpluu@gmail.com>
2025-07-31 18:53:31 +0700

    feat: add form

--- src/example/form.tsx
+++ src/example/form.tsx
@@ -1,21 +1,31 @@
 import {type ChangeEvent, type FormEventHandler, useState,} from "react";
-import {useDebounce} from "../hooks/useDebounce";
+import {delay} from "../utils/delay.ts";
+import {LoaderCircle} from "lucide-react";
+
+const submitForm = async (formData: FormData) => {
+    await delay(1000)
+    if (formData.username.includes("test")) {
+        throw new Error("Failed form submission")
+    } else {
+        return formData;
+    }
+};
+
+interface FormData {
+    username: string;
+    password: string;
+}
 
 /**
  * Controlled Component
- * Handle debounce
  */
 export const Form = () => {
-    const submitForm = () => {
-        console.log(formData);
-    };
-
-    const debouncedSubmitForm = useDebounce(submitForm);
-
     const [formData, setFormData] = useState({
         username: "",
-        password: "",
+        password: ""
     });
+    const [errorMessage, setErrorMessage] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
 
     const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
         setFormData({
@@ -24,33 +34,51 @@ export const Form = () => {
         });
     };
 
-    const handleSubmit: FormEventHandler = (event) => {
+    const handleSubmit: FormEventHandler = async (event) => {
         event.preventDefault();
+        setIsLoading(true)
         // Call the last handler
-        debouncedSubmitForm();
+        try {
+            await submitForm(formData)
+            setFormData({
+                username: "",
+                password: ""
+            })
+        } catch (error: unknown) {
+            setErrorMessage(error.message)
+        } finally {
+            setIsLoading(false)
+        }
     };
 
+    if (isLoading) {
+        return <LoaderCircle/>
+    }
+
     return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor={"username"}>Username</label>
-            <input
-                type="text"
-                id={"username"}
-                name={"username"}
-                value={formData.username}
-                onChange={handleChange}
-            />
-            <br/>
-            <label htmlFor={"password"}>Password</label>
-            <input
-                type="password"
-                id={"password"}
-                name={"password"}
-                value={formData.password}
-                onChange={handleChange}
-            />
-            <br/>
-            <input type="submit"/>
-        </form>
+        <>
+            <span>{errorMessage}</span>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor={"username"}>Username</label>
+                <input
+                    type="text"
+                    id={"username"}
+                    name={"username"}
+                    value={formData.username}
+                    onChange={handleChange}
+                />
+                <br/>
+                <label htmlFor={"password"}>Password</label>
+                <input
+                    type="password"
+                    id={"password"}
+                    name={"password"}
+                    value={formData.password}
+                    onChange={handleChange}
+                />
+                <br/>
+                <input type="submit"/>
+            </form>
+        </>
     );
 };
--- src/example/product-table/FilteredProducTable.tsx
+++ src/example/product-table/FilteredProducTable.tsx
@@ -3,15 +3,17 @@ import {ProductTable} from "./ProductTable.tsx";
 import {SearchBar} from "./SearchBar.tsx";
 import type {Product} from "./data.tsx";
 
-
 export const FilteredProductTable = ({products}: { products: Product[] }) => {
     const [searchTerm, setSearchTerm] = useState("")
     const [inStocked, setInStocked] = useState(false)
 
-
     return <>
-        <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} inStocked={inStocked}
-                   setInStocked={setInStocked}/>
+        <SearchBar
+            searchTerm={searchTerm}
+            setSearchTerm={setSearchTerm}
+            inStocked={inStocked}
+            setInStocked={setInStocked}
+        />
         <ProductTable
             products={products
                 .filter(product => product.name.toLowerCase().includes(searchTerm.toLowerCase()))
@@ -20,3 +22,6 @@ export const FilteredProductTable = ({products}: { products: Product[] }) => {
     </>
 }
 
+function customReduce = (array: Array<any>) => {
+
+}
