f69bc8c737302c65ce011ab6570aea52b463957b
lphuynh <huynhpluu@gmail.com>
2025-08-02 01:13:11 +0700

    feat: remove unused code

--- src/example/product-table/FilteredProducTable.tsx
+++ src/example/product-table/FilteredProducTable.tsx
@@ -1,27 +1,27 @@
-import {useState} from "react";
-import {ProductTable} from "./ProductTable.tsx";
-import {SearchBar} from "./SearchBar.tsx";
-import type {Product} from "./data.tsx";
+import { useState } from "react";
+import { ProductTable } from "./ProductTable.tsx";
+import { SearchBar } from "./SearchBar.tsx";
+import type { Product } from "./data.tsx";
 
-export const FilteredProductTable = ({products}: { products: Product[] }) => {
-    const [searchTerm, setSearchTerm] = useState("")
-    const [inStocked, setInStocked] = useState(false)
+export const FilteredProductTable = ({ products }: { products: Product[] }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [inStocked, setInStocked] = useState(false);
 
-    return <>
-        <SearchBar
-            searchTerm={searchTerm}
-            setSearchTerm={setSearchTerm}
-            inStocked={inStocked}
-            setInStocked={setInStocked}
-        />
-        <ProductTable
-            products={products
-                .filter(product => product.name.toLowerCase().includes(searchTerm.toLowerCase()))
-                .filter(product => inStocked ? product.stocked : true)
-            }/>
+  return (
+    <>
+      <SearchBar
+        searchTerm={searchTerm}
+        setSearchTerm={setSearchTerm}
+        inStocked={inStocked}
+        setInStocked={setInStocked}
+      />
+      <ProductTable
+        products={products
+          .filter((product) =>
+            product.name.toLowerCase().includes(searchTerm.toLowerCase())
+          )
+          .filter((product) => (inStocked ? product.stocked : true))}
+      />
     </>
-}
-
-function customReduce = (array: Array<any>) => {
-
-}
+  );
+};
