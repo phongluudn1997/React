5e10af431c21670c747e906236648017166092dd
lphuynh <huynhpluu@gmail.com>
2025-08-21 17:39:53 +0700

    feat: update useQuery

--- src/hooks/useQuery.ts
+++ src/hooks/useQuery.ts
@@ -1,56 +1,75 @@
 import {useEffect, useReducer} from "react";
 
 interface UseQueryOptions<TData> {
-    initialData?: TData
+    initialData?: TData;
 }
 
 export function useQuery<TData = unknown, TError = unknown>(
     queryFn: () => Promise<TData>,
     options?: UseQueryOptions<TData>
 ) {
-    const [state, dispatch] = useReducer<QueryState<TData, TError>, [QueryAction<TData, TError>]>(dataFetchReducer, {
+    const [state, dispatch] = useReducer<
+        QueryState<TData, TError>,
+        [QueryAction<TData, TError>]
+    >(dataFetchReducer, {
         data: options?.initialData,
-        loading: false,
-        error: undefined
-    })
+        status: "idle",
+        error: undefined,
+    });
 
     useEffect(() => {
         async function fetchData() {
-            dispatch({type: "FETCH_START"})
+            dispatch({type: "FETCH_START"});
             try {
-                const result = await queryFn()
-                dispatch({type: "FETCH_SUCCESS", payload: result})
+                const result = await queryFn();
+                dispatch({type: "FETCH_SUCCESS", payload: result});
             } catch (error) {
-                dispatch({type: "FETCH_FAILED", payload: error as TError})
+                dispatch({type: "FETCH_FAILED", payload: error as TError});
             }
         }
 
         fetchData();
     }, [queryFn]);
 
-    return state
-};
+
+    return {
+        ...state,
+        isLoading: state.status === "loading",
+        isError: state.status === "error",
+        isSuccess: state.status === "success"
+    };
+}
+
+type Status = "idle" | "loading" | "error" | "success"
 
 interface QueryState<TData, TError> {
     data: TData | undefined;
-    loading: boolean;
-    error: TError | undefined
+    status: Status
+    error: TError | undefined;
 }
 
 type QueryAction<TData, TError> =
-    { type: "FETCH_START" } |
-    { type: "FETCH_SUCCESS", payload: TData } |
-    { type: "FETCH_FAILED", payload: TError }
+    | { type: "FETCH_START" }
+    | { type: "FETCH_SUCCESS"; payload: TData }
+    | { type: "FETCH_FAILED"; payload: TError };
 
-function dataFetchReducer<TData, TError>(state: QueryState<TData, TError>, action: QueryAction<TData, TError>): QueryState<TData, TError> {
+function dataFetchReducer<TData, TError>(
+    state: QueryState<TData, TError>,
+    action: QueryAction<TData, TError>
+): QueryState<TData, TError> {
     switch (action.type) {
         case "FETCH_START":
-            return {...state, loading: true, error: undefined};
+            return {...state, status: "loading", error: undefined};
         case "FETCH_SUCCESS":
-            return {...state, data: action.payload, loading: false, error: undefined};
+            return {
+                ...state,
+                data: action.payload,
+                status: "success",
+                error: undefined,
+            };
         case "FETCH_FAILED":
-            return {...state, loading: false, error: action.payload}
+            return {...state, status: "error", error: action.payload};
         default:
-            return state
+            return state;
     }
 }
